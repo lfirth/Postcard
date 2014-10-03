8e61ae1f05a09fddc99da03744eed31b1c6cf28d
Lee Firth <lee@firthwebworks.com.au>
2014-10-03 22:14:10 +1000

    Initial Commit

---
+++ Postcard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Postcard
+//
+//  Created by Lee Firth on 3/10/2014.
+//  Copyright (c) 2014 Firth Web Works. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
